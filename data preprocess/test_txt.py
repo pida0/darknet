6d0157505661acad376c8013610c3b62ec986999
pida0 <shuying97@163.com>
2020-01-04 17:04:59 +0800

    Update test_txt.py

--- data preprocess/test_txt.py
+++ data preprocess/test_txt.py
@@ -3,7 +3,7 @@ import os
 from os import listdir, getcwd
 from os.path import join
 if __name__ == '__main__':  # 只有在文件作为脚本文件直接执行时才执行下面代码
-    source_folder='/home/sy/DataSet/cv-project/示例/示例/testimage/' #图片保存的路径
+    source_folder='/home/sy/DataSet/cv-project/示例/示例/testimage/' #测试图片的路径
     dest='/home/sy/DataSet/cv-project/示例/示例/test.txt' #写有图片的名字的路径
     file_list=os.listdir(source_folder)#获取各图片的名称       
     test_file=open(dest,'a')    #追加写打开              
